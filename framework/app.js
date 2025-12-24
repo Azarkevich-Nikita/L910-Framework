2bb3703f13e260fe3c8b5bffcc7abd9a44711079
Nikita <mykesvek@gmail.com>
2025-12-24 17:21:58 +0300

    feat: Unneeded parts of the app have been removed.

--- framework/app.js
+++ framework/app.js
@@ -69,26 +69,3 @@ class App{
 }
 
 module.exports = App;
-
-
-// const port = 5050
-
-// const server = http.createServer(function(req, res){
-//     try{
-//         console.log('Now we connect with:', req.url)
-//         res.write('Now we are working about it!')
-//         res.end()
-//     }
-//     catch(error){
-//         console.log('Something wrong', error)
-//     }
-// })
-
-// server.listen(port, function(error){
-//     if(error){
-//         console.log('Something went wrong!', error)
-//     }
-//     else{
-//         console.log('Server is litening on port', port)
-//     }
-// })
